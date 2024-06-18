b785de8cfe4985e2b57d5a432164d8676cb72497
jonasfr <jonasgouveia19@gmail.com>
2024-06-17 21:41:21 -0400

    Modificação no arquivo de ponteiros

--- Curso/8-Ponteiros/pont.go
+++ Curso/8-Ponteiros/pont.go
@@ -7,14 +7,14 @@ func main() {
 	fmt.Println("Ponteiros")
 	var var1 int = 10
 	var var2 int = var1
-
 	fmt.Println(var1, var2)
-
 	var1++
+	fmt.Println(var1, var2)
 
 	//Ponteiro é uma referência de memória
 	//Guarda um valor inteiro
 	var var3 int = 150
+	var3++
 	//Guarda o endereço de memória do valor inteiro
 	var pont1 *int = &var3
 	fmt.Println(var3, pont1)
